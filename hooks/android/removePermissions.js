b302932bffa8d4e5874abd7cc12423827e54e12c
SergioAlexandreTrindade <126238381+SergioAlexandreTrindade@users.noreply.github.com>
2023-04-20 10:17:13 +0100

    Update removePermissions.js

--- hooks/android/removePermissions.js
+++ hooks/android/removePermissions.js
@@ -1,22 +1,25 @@
-var permissionsToRemove = [ "com.google.android.gms.permission.AD_ID"]; 
-
+var permissionsToRemove = ["com.google.android.gms.permission.AD_ID"]; 
 var fs = require('fs'); 
-var path = require('path'); 
-var rootdir = ""; 
-var manifestFile = path.join(rootdir, "platforms/android/app/src/main/AndroidManifest.xml"); 
+var path = require('path');
 
-fs.readFile( manifestFile, "utf8", function( err, data ) 
-{ 
+module.exports = function (context) {
+    var projectRoot = context.opts.cordova.project ? context.opts.cordova.project.root : context.opts.projectRoot;
+    var manifestPath = path.join(projectRoot, 'platforms/android/app/src/main/AndroidManifest.xml');
+    var manifestFile = fs.readFileSync(manifestPath).toString();
+    
+    fs.readFile( manifestFile, "utf8", function( err, data ){ 
     if (err) 
         return console.log( err ); 
 
     var result = data; 
     for (var i=0; i<permissionsToRemove.length; i++) 
-        result = result.replace( "<uses-permission android:name=\"" + permissionsToRemove[i] + "\"/>", "" );  
+        result = result.replace( "<uses-permission android:name=\"" + permissionsToRemove[i] + "\"/>", "" ); 
 
     fs.writeFile( manifestFile, result, "utf8", function( err ) 
     { 
         if (err) 
             return console.log( err ); 
     } ); 
-} );
+    } );
+
+};
